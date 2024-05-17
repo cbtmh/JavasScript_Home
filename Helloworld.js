fe30b450ce514a39694c6d1fd9aff19dbe99a589
Vu <Vu@gmail.com>
2024-05-17 19:49:50 +0700

    add day 1

---
+++ Helloworld.js
@@ -0,0 +1,24 @@
+function run(){
+    console.log("Hello World");
+}
+
+alert("Hello World");
+
+var a = 1;
+var b = -2;
+var c = a+b;
+var d = a-b;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+var firstName = 'Kiều';
+var lastName = 'Trang';
+var fullName = firstName + ' ' +lastName
+console.log(fullName);
+
+var age = 16
+var canBuyAlcohol = age>18;
+console.log(canBuyAlcohol)
+
